6a0432c0119d356953752e793a0df292f6fdc6db
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2019-11-24 12:40:16 +0000

    Clean up - array reverse in place

--- src/reverseArrayInPlace.js
+++ src/reverseArrayInPlace.js
@@ -3,9 +3,8 @@ function arrRevInPlace(arr){
 	for (var i = 0; i < arr.length/2; i++) {
 		var tmp1 = arr[i];
 		var revItemIdx = arr.length - 1 - i;
-		var tmp2 = arr[revItemIdx];
 		
-		arr[i] = tmp2;
+		arr[i] = arr[revItemIdx];
 		arr[revItemIdx] = tmp1;
 		
 	}
